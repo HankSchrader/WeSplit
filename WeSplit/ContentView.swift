cb036d88c8854385b44c1f6e2ee5131b6fa87ac9
Erik Mikac <mikacew@gmail.com>
2020-02-05 11:59:20 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sara Nicole Mikac on 2/5/20.
+//  Copyright © 2020 Erik Mikac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
