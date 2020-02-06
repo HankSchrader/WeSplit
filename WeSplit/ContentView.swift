84526942f8266bc082a667d94c44e72dfcc4e1ae
Erik Mikac <mikacew@gmail.com>
2020-02-05 19:18:25 -0500

    WeSplit App - Added Grand Total section.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,34 +12,56 @@ import SwiftUI
 
 struct ContentView: View {
     // @State watches for changes, and automatically reloads the body.
-    @State private var checkoutAmount = ""
+    @State private var checkAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
-
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    var grandTotal: Double {
+         let tipSelection = Double(tipPercentages[tipPercentage])
+         let orderAmount = Double(checkAmount) ?? 0
+         let tipValue = orderAmount / 100 * tipSelection
+         return orderAmount + tipValue
+    }
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkoutAmount)
-                    .keyboardType(.decimalPad)
-                Picker("Number of people:", selection: $numberOfPeople) {
-                    ForEach(2..<100) {
-                        Text("\($0) people")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people:", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                            }
+                        }
+                }
+                Section {
+                    Picker("Tip: ", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
                         }
                     }
-            }
-            Section {
-                Picker("Tip: ", selection: $tipPercentage) {
-                    ForEach(0..<tipPercentages.count) {
-                        Text("\(self.tipPercentages[$0])%")
-                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
-                .pickerStyle(SegmentedPickerStyle())
-            }
-            Section {
-                Text("Check Amount: $\(checkoutAmount).")
-            }
+                
+                Section(header: Text("Grand Total")) {
+                      Text("Grand Total: $\(grandTotal, specifier: "%.2f").")
+                }
+            
+                Section(header: Text("Amount Per Person")) {
+                      Text("Total Per Person: $\(totalPerPerson, specifier: "%.2f").")
+                }
+            }.navigationBarTitle("WeSplit")
         }
     }
 }
